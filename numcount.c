ba6fd435800175783535ef39f89ad72ab425cb56
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-02-05 16:27:26 +0530

    Create numcount.c

---
+++ numcount.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+    int main()
+{
+    char    a[100];
+    int d=0,i;
+    gets(a);
+ for(i=0;a[i]!=NULL;i++)
+    {
+ 
+        if(a[i]>='0' && a[i]<='9')
+        {
+            d++;
+        }
+    }
+      printf("%d",d);
+    
+ 
+    return 0;
+}
