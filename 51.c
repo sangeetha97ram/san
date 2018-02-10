f45dda266fbb5607b00303a6972a51e533c51f7c
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-02-10 23:35:49 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,rem,r=0,a;
+scanf("%d",&n);
+while(n!=0)
+{
+rem=n%10;
+r=r*10+rem;
+n=n/10;
+}
+while(r!=0)
+{
+a=r%10;
+printf("%d\t",a);
+r=r/10;
+}
+return 0;
+}
