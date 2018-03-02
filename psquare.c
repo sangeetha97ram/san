556e3b3c7d58a69dc6e9427b5fe53f09d85649da
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-02 16:10:19 +0530

    Create psquare.c

---
+++ psquare.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	scanf("%d  %d",&a,&b);
+	c=a*b;
+	
+		if(c==(a*a))
+		{
+			printf("yes");
+		}
+		else
+		{
+			printf("no");
+		}
+	return 0;
+}
