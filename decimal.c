35c7d82f6ef3e448608043f3df184f2e8679b409
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-12 16:05:39 +0530

    Create decimal.c

---
+++ decimal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float length,breadth;
+    scanf("%f %f",&length,&breadth);
+    float area;
+    area=length*breadth;
+    printf("%.5f",area);
+    return 0;
+}
