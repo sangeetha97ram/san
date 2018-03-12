eea6f11d68303b512070da7d70c2f73904443f5e
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-12 16:02:08 +0530

    Create deci.c

---
+++ deci.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float length,breadth;
+    scanf("%f %f",&length,&breadth);
+    float area;
+    area=length*breadth;
+    printf("%f",area);
+    return 0;
+}
