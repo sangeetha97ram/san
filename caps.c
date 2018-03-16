1f4da7521ca5d6276592ce5cbe7cf3bcd7c51ea0
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-16 16:53:43 +0530

    Create caps.c

---
+++ caps.c
@@ -0,0 +1,19 @@
+#include<string.h>
+ int main()
+ {
+ 	int n,i;
+ 	char s[100];
+ 	gets(s);
+ 	n=strlen(s);
+ 	s[0]=s[0]-32;
+ 	for(i=0;i<n;i++)
+ 	{
+ 		if(s[i]==' ')
+   {
+    if((s[i+1]>='a')&&(s[i+1]<='z'))
+ 		s[i+1]=s[i+1]-32;
+   }
+ 	}
+ 	printf("%s",s);
+ 	return 0;
+ }
