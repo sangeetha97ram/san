a32eea60a771283763bc8a89eacc83eb6a3d4677
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-19 15:52:35 +0530

    Create minimum.c

---
+++ minimum.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+int main()
+{
+int i,j,a[10],temp,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n-1;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d",a[0]);
+return 0;
+}
