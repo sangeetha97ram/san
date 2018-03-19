42bbee134e077d431abc3796fee79d8aa60fdac9
sangeetha97ram <31505716+sangeetha97ram@users.noreply.github.com>
2018-03-19 16:34:54 +0530

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int c=0,i,j,a;
+  char str[10];
+  scanf("%s",&str);
+  a=strlen(str);
+  for(i=0;i<a;i++)
+  {
+    for(j=i+1;j<=a;j++)
+    {
+      if(str[i]==str[j])
+      {
+        c++;
+      }
+    }
+  }
+  if(c==0)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("No");
+  }
+  return 0;
+}
